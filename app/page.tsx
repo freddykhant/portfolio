b7e0679e761dcb5f82bad35aef1941a1223d9b55
Freddy Khant <freddyhmk@gmail.com>
2025-02-17 22:21:56 +0800

    projects list

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,36 @@
 import Image from "next/image";
+import { Github, Linkedin, Mail } from "lucide-react";
+
+const projects = [
+  {
+    name: "ai-chatbot",
+    description: "AI chatbot with RAG + web scraping capabilities 🤖",
+    link: "https://github.com/freddykhant/ai-chatbot",
+  },
+  {
+    name: "flex-ai",
+    description:
+      "flex-ai is your personal trainer and chatbot in one, leveraging an RAG pipeline to retrieve info from academic research to answer your fitness questions💪",
+    link: "https://github.com/freddykhant/flex-ai",
+  },
+  {
+    name: "summary-ai",
+    description:
+      "summary-ai is your intelligent csv analyser and summariser, using a RAG pipeline to extract key insights from your data files and provide concise, ai-powered summaries and insights 📊🧠",
+    link: "https://github.com/freddykhant/summary-ai",
+  },
+  {
+    name: "buildshare",
+    description:
+      "a social media for tech setups, built as part of buildspace's s4 nights & weekends program",
+    link: "https://github.com/freddykhant/buildshare_app",
+  },
+  {
+    name: "portfolio",
+    description: "this website right here 😁",
+    link: "https://github.com/freddykhant/portfolio",
+  },
+];
 
 export default function Home() {
   return (
