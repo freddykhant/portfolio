4d3ef2b06a6ebec1fbc6fd514605424a6f10a774
Freddy Khant <freddyhmk@gmail.com>
2025-02-17 17:51:52 +0800

    projects (bare bones content)

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,13 @@ export default function Home() {
         hi, my name is freddy. i'm a 22 year old comp sci student with a passion
         for building cool things. look around 😁
       </p>
+      <h2> Projects </h2>
+      <ul>
+        <li>ai-chatbot</li>
+        <li>flex-ai</li>
+        <li>summary-ai</li>
+        <li>buildshare</li>
+      </ul>
     </main>
   );
 }
