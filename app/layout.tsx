0c5feb44357c0b2e5e62a2585ab3afa85c6715cc
Freddy Khant <freddyhmk@gmail.com>
2025-02-17 21:17:55 +0800

    site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata = {
+  title: "Freddy Khant",
+  description: "a website about freddy khant",
+};
+
 export default function RootLayout({
   children,
 }: {
